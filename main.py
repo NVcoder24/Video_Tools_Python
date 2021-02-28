c6141b270064439fe4af8c1a682da69ffbcfd344
Coder <60108915+NVcoder24@users.noreply.github.com>
2021-02-28 12:41:53 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,48 +2,48 @@ import cv2
 import os
 from config import tools_cfg
 
-show_p = tools_cfg["Base_cfg"]["info"]["show_process"]
-print_p = tools_cfg["Base_cfg"]["info"]["print_process"]
+fps = tools_cfg["FTV_cfg"]["video_info"]["fps"]
 
-vidcap = cv2.VideoCapture(f"{tools_cfg['VTF_cfg']['paths']['file_name']}.{tools_cfg['VTF_cfg']['paths']['format']}")
-success,image = vidcap.read()
-count = 0
-count_ = 1
-success = True
-path = tools_cfg['VTF_cfg']['paths']['frames_path']
-length = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
-fps = vidcap.get(cv2.CAP_PROP_FPS)
-height = vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-width = vidcap.get(cv2.CAP_PROP_FRAME_WIDTH)
+files = []
 
-iff = lambda value : str(value)[:str(value).index('.')]
-find_procent = lambda value, max : value * 100 / max
+path = tools_cfg["FTV_cfg"]["paths"]["frames_path"]
+i = 0
 
-length_sec = iff(length / fps)
+print("--- FINDING FRAMES ---")
+while True:
+    if os.path.isfile(f"{path}frame_{i}.png"):
+        files.append(f"{path}frame_{i}.png")
+        print(f"Found frame: frame_{i}.png")
+        i += 1
+    else:
+        break
 
-try:
-    os.mkdir(path)
-except Exception as e:
-    pass
+i = 0
 
-print(f"--- VIDEO INFORMATION ---\nvideo length (in frames): {length}\nvideo FPS: {fps}\nvideo length (in sec): {length_sec}\nvideo resolution: {iff(width)}px/{iff(height)}px ({iff(height)}p)\n-------------------------")
-input("Press Enter to begin!")
+frames = []
 
-
-while success:
-    success,image = vidcap.read()
+print("--- WRITING FRAMES IN RAM ---")
+for file in files:
     try:
-        cv2.imwrite(f"{path}frame_{count}.png", image)
-        count__ = find_procent(count_, length)
-        if print_p:
-            print(f"Frame {count} saved! ({iff(count__)}%)")
-        if show_p:
-            cv2.imshow(f"Current frame", image)
+        img = cv2.imread(file)
+        cv2.imshow("Current frame - writing frames in RAM", img)
         if cv2.waitKey(10) == 27:
             cv2.destroyAllWindows()
             break
-        count += 1
-        count_ += 1
+        height, width, layers = img.shape
+        size = (width, height)
+        frames.append(img)
+        print(f"Writing frame in RAM: {i} ({file})")
+        i += 1
     except Exception as e:
-        print("Finish!")
-        quit(666)
+        pass
+
+print("--- WRITING VIDEO ---")
+result = cv2.VideoWriter(f"{tools_cfg['FTV_cfg']['paths']['file_name']}.{tools_cfg['FTV_cfg']['paths']['format']}",cv2.VideoWriter_fourcc(*f"{tools_cfg['FTV_cfg']['video_info']['codec']}"), fps, size)
+
+for frame in range(len(frames)):
+    print(f"Writing frame in video file: {frame}")
+    result.write(frames[frame])
+
+print("Finish!")
+quit(666)
